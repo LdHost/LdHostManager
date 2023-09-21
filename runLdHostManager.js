b5030df8b3af625680c8dae6bc554f2f0febfcc4
Eric Prud'hommeaux <eric+github@w3.org>
2023-09-22 00:56:10 +0200

    + createSite test

--- runLdHostManager.js
+++ runLdHostManager.js
@@ -4,6 +4,9 @@ const {LdHostManagementServer} = require("./LdHostManagerServer");
 const defaultPort = "3000";
 const defaultConfigPath = "./LdHost.config.json";
 
+process.on('SIGINT', () => {process.exit(2);});
+process.on('SIGQUIT', () => {process.exit(1);});
+
 const [undefined, Argv0, portParam, configPathParam] = process.argv;
 new LdHostManagementServer(
   portParam || process.env.PORT || defaultPort,
--- test/LdHostManager-curl.test.js
+++ test/LdHostManager-curl.test.js
@@ -4,18 +4,21 @@
 (base) PS1=16:54:56-eric@touchy:/tmp/toy/vue-js-client-crud$ curl -X POST -s http://localhost:3001/updateSubdomain -d type=github -d org=cotton -d repo=Cotton -d subdomain=cotton
 {"actions":["linked cotton to github/StaticFDP/Cotton"]}
 */
+const Fs = require('fs');
+const Path = require('path');
 const Cp = require('child_process');
+const debug = require('debug')('test:LdHostManager-curl');
 const {ExpectProcessOutput} = require('./ExpectProcessOutput');
 let ServerPort = null;
 let time = new Date();
 const Server = new ExpectProcessOutput(Cp.spawn(
-  './runLdHostManager-toy.js',
-  ['3002', './LdHost-test.config.json'],
-  {env: {'DEBUG': '*'}}
-));
+  './runLdHostManager.js',
+  ['3002', './test/LdHost-test.config.json'],
+  {env: {'DEBUG': '*', 'PATH': process.env.PATH}}
+), debug);
 
 beforeAll(async () => {
-  const m = await Server.expectErr(/^(  backend:server Listening on port (\d+) \+\d+ms)\n/);
+  const m = await Server.expectErr(/(backend:server Listening on port (\d+).*\n)/);
   ServerPort = parseInt(m[2]);
 });
 
@@ -35,8 +38,65 @@ describe('LdHostManager', () => {
     expect(ServerPort).toEqual(3002);
   });
 
-  it('should see', async () => {
-    expect((await Server.expectErr(/more stuff\n/))[0]).toEqual('more stuff\n');
+  it('should GET sites', async () => {
+    const curl = Cp.spawnSync(
+      'curl',
+      [
+        "-X", "GET",
+        "-s", `http://localhost:${ServerPort}/sites`
+      ],
+      { encoding : 'utf8' }
+    );
+    const res = JSON.parse(curl.stdout);
+    expect(res.subdomains).toEqual( [ {
+        "DocumentRoot": "/home/fdpCloud/sites/github/StaticFDP/wikidata",
+        "ServerName": "wikidata.fdpcloud.org",
+    } ] );
+    expect(res.sites.sort((l, r) => l.repo.localeCompare(r))).toEqual(
+      [
+        { "dateTime": "2023-09-15T15:24:22.000Z",
+          "hash": "3285971ea73195c7940519c55652530392c651bb",
+          "org": "StaticFDP",
+          "repo": "FlashCard1",
+          "sitePath": "github/StaticFDP/FlashCard1",
+          "type": "github",
+          "who": "GitHub" },
+        { "dateTime": "2023-09-15T08:09:50.000Z",
+          "hash": "882a283c7baf83030f2ab697a4edf1eb5db6420b",
+          "org": "StaticFDP",
+          "repo": "wikidata",
+          "sitePath": "github/StaticFDP/wikidata",
+          "subdomain": "wikidata",
+          "type": "github",
+          "who": "Eric Prud'hommeaux" }
+      ] );
+    expect(curl.stderr).toEqual("");
+    expect(curl.status).toEqual(0);
+    const serverLog = (await Server.expectOut(/^(.*wikidata.*)$/s))[0];
+    expect(serverLog).toEqual("{ 'github/StaticFDP/wikidata': 'wikidata' }\n");
+  });
+
+  it('should call createSite', async () => {
+    const curl = Cp.spawnSync(
+      'curl',
+      [
+        "-X", "POST",
+        "-s", `http://localhost:${ServerPort}/createSite`,
+        "-d", "type=github",
+        "-d", "org=StaticFDP",
+        "-d", "repo=Cotton"
+      ]
+    );
+    expect(JSON.parse(curl.stdout)).toEqual(
+      {"actions":[
+        "cloned http://github.com/StaticFDP/Cotton to github/StaticFDP/Cotton"
+      ]}
+    );
+    expect(curl.stderr.toString()).toEqual('');
+    expect(curl.status).toEqual(0);
+    expect((await Server.expectErr(/(cloned [^ ]+ to [^ ]+\n)/))[0]).toMatch(/cloned [^ ]+ to [^ ]+\n/);
+    const repo = Path.join(__dirname, "root/home/fdpCloud/sites/github/StaticFDP/Cotton");
+    console.log(await Fs.promises.unlink(repo));
   });
 
   it('should end', async () => {
@@ -45,4 +105,3 @@ describe('LdHostManager', () => {
   });
 });
 
-// This script exits much quicker
